57a16b3357f768e5904a846d08a27e588d420387
thallkeer <kir730@yandex.ru>
2018-04-06 18:17:22 +0400

    added sha-256 realisation

---
+++ ClientForLab/ClientForLab/CipherUtils/PrivateKey.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace ClientForLab.CipherUtils
+{
+    public class PrivateKey
+    {
+        public BigInteger D { get; }
+        public BigInteger N { get; }
+
+        public PrivateKey(BigInteger d, BigInteger n)
+        {
+            this.D = d;
+            this.N = n;
+        }
+    }
+}
